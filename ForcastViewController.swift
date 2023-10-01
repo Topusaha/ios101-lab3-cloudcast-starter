396f4562094cee83ead895db9d7ec3704b663c0f
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-10-01 12:21:38 -0400

    Created sample demo

---
+++ ForcastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ForcastViewController.swift
+//  CloudCast
+//
+//  Created by Topu Saha on 9/26/23.
+//
+
+import UIKit
+
+class ForcastViewController: UIViewController {
+    // Function override for the view controller
+    // This is fired when the view has finished loading for the first time
+    override func viewDidLoad() {
+        // Some functions require you to call the super class implementation
+        // Always read the online documentation to know if you need to
+        super.viewDidLoad()
+        }
+}
